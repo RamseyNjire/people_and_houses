8b21ab2cff983779ba97f44bd90743eebe316f4d
Ramsey Njire <ramseynjire@gmail.com>
2021-03-25 11:20:13 +0300

    create house model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_074005) do
+ActiveRecord::Schema.define(version: 2021_03_25_081851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "address", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name", null: false
